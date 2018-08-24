ce65d3bfc7281dc1ca512871bdb15c72ac94d081
Mike Borozdin <mborozdin@ethoslending.com>
2018-08-24 16:25:43 -0700

    logging, output, request to process

--- src/docutech.client/Client.cs
+++ src/docutech.client/Client.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Text;
+using System.Collections.Specialized;
+using Microsoft.Extensions.Logging;
 
 
 namespace docutech.client
@@ -9,28 +13,62 @@ namespace docutech.client
     public class Client
     {
 		HttpClient client = new HttpClient();
+		private readonly ILogger<Client> _logger;
 
-        public Client(string baseAddress)
+        public Client(ILogger<Client> logger)
 		{
-			client.BaseAddress = new Uri(baseAddress);
+			_logger = logger;
+		}
+
+		// <summary>
+		// set the base address of the Client.
+		// </summary>
+		public string BaseAddress 
+		{
+			set  { client.BaseAddress = new Uri(value); }
+			get { return client.BaseAddress.AbsoluteUri; }
 		}
 
 		/// <summary>
-		/// an import function that will kick off the import process into DocuTech
+		/// the main function that will kick off the process into DocuTech
 		/// </summary>
-		public async Task<bool> Import(string importXml)
+		public async Task<bool> Process(string xml, string baseAddress = null)
 		{
-			const string importUrl = "cx4/cxws/enginews.asmx/Import";
-			var content = new StringContent(importXml, Encoding.UTF8, "text/xml");
-			var result = await client.PostAsync(importUrl, content);
+			BaseAddress = baseAddress ?? BaseAddress;
+			
+			const string processUrl = "cx4/cxws/enginews.asmx";
+			
+			var encoded = WebUtility.UrlEncode(xml);
+			var requestBody = $@"<?xml version=""1.0"" encoding=""utf-8""?>
+				<soap:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"">
+				<soap:Body>
+					<Process xmlns=""http://conformx.docutechcorp.com/ConformXWS"">
+					<xmlRequest>{encoded}</xmlRequest>
+					</Process>
+				</soap:Body>
+				</soap:Envelope>";
+
+			var content = new StringContent(requestBody);
+			content.Headers.ContentType = new MediaTypeHeaderValue("text/xml");
+			content.Headers.ContentEncoding.Add("UTF-8");
+			content.Headers.Add("SOAPAction", "http://conformx.docutechcorp.com/ConformXWS/Process");
+
+			var result = await client.PostAsync(processUrl, content);
+			
+			// TODO: this should not be logged in the production version.
+			var rbody = await result.Content.ReadAsStringAsync();
+			_logger.LogInformation(rbody);
+
 			return result.IsSuccessStatusCode;
 		}
 
 		/// <summary>
 		/// a basic ping function that calls "Hello World"
 		/// </summary>
-		public async Task<bool> Ping()
+		public async Task<bool> Ping(string baseAddress = null)
 		{
+			BaseAddress = baseAddress ?? BaseAddress;
+
 			const string pingUrl = "cx4/cxws/enginews.asmx/HelloWorld";
 			const string pingXml = @"
 						<?xml version=""1.0"" encoding=""utf-8""?>
@@ -41,6 +79,7 @@ namespace docutech.client
 						</soap:Envelope>";
 
 			var content = new StringContent(pingXml, Encoding.UTF8, "text/xml");
+			content.Headers.Add("SOAPAction", "http://conformx.docutechcorp.com/ConformXWS/HelloWorld");
 			var result = await client.PostAsync(pingUrl, content);
 			return result.IsSuccessStatusCode;
 		}
--- test/docutech.test/BasicTest.cs
+++ test/docutech.test/BasicTest.cs
@@ -1,6 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Threading.Tasks;
 using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 using docutech.client;
 
@@ -12,25 +14,39 @@ namespace docutech.test
     {
         private const string STAGE_URL = "https://stage.conformx.com";
         public TestContext TestContext {get;set;}
+        private ILoggerFactory _loggerFactory;
+
+        [TestInitialize]
+        public void Init()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .BuildServiceProvider();
+
+            _loggerFactory = serviceProvider.GetService<ILoggerFactory>().AddConsole().AddDebug();
+        }
 
         [TestMethod]
         public async Task TestPing()
         {
-            var client = new Client(STAGE_URL);
-            var result = await client.Ping();
+            var logger = _loggerFactory.CreateLogger<Client>();
+            var client = new Client(logger);
+            var result = await client.Ping(STAGE_URL);
             Assert.IsTrue(result);
         }
 
         [TestMethod]
-        public async Task TestImport()
+        public async Task TestProcess()
         {
-            var client = new Client(STAGE_URL);
+            var logger = _loggerFactory.CreateLogger<Client>();
+            var client = new Client(logger);
+
             using (StreamReader stream = new StreamReader("test_data/init_disc_lights_on.xml"))
             {
                 string importXml = stream.ReadToEnd();
                 TestContext.WriteLine($"XML file content: {importXml.Substring(0, 60)}...");
 
-                var result = await client.Import(importXml);
+                var result = await client.Process(importXml, STAGE_URL);
                 Assert.IsTrue(result);
             }
         }
